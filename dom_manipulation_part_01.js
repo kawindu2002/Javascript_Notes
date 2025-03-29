a54a4863dd4988031374f900e0eb114e0b4fd8c0
kawindu2002 <kawindupriyashan@gmail.com>
2025-03-29 15:06:04 +0530

    add dom manipulation

---
+++ dom_manipulation_part_01.js
@@ -0,0 +1,15 @@
+/**
+    document = DOM
+    document.getElementsBytagName(h1");
+
+    var h1_elements = document.getElementsByTagName("h1");
+    h1_elements[0]
+        Output -->   <h1>Welcome to Javascript 01</h1>
+
+    var h1_elements = document.getElementsByTagName("h1");
+    h1_elements[1]
+        Output -->   <h1>Welcome to Javascript 02</h1>
+
+
+*/
+
