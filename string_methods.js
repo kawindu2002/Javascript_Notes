e41af219825dfa905534a6f6caaac4e24588f4d5
kawindu2002 <kawindupriyashan@gmail.com>
2025-04-05 09:58:58 +0530

    add replace() js

--- string_methods.js
+++ string_methods.js
@@ -1,30 +1,36 @@
-let str1 = "Hello World";
-console.log(str1.charAt(0)); // 'H'
+// let str1 = "Hello World";
+// console.log(str1.charAt(0)); // 'H'
+//
+// console.log(str1.charCodeAt(0)); // 104
+//
+// console.log(str1.charCodeAt(5)); // 44
+//
+//
+// let str1 = "H";
+// let str2 = "ello";
+// let strValue = str1 + str2;
+//
+// let starValue = str1.concat(str2);
+// console.log(starValue);
+//
+// let mystr = "www.ijse.lk";
+// mystr.startsWith("www"); // true
+// mystr.startsWith("https"); // false
+//
+// mystr.endsWith("com"); // false
+// mystr.endsWith("lk"); // true
+//
+// let newStr1 = Hello;
+// let newStr2 = Hello;
+// let newStr3 = Hii;
+//
+// newStr1.localeCompare(newStr2); // 0
+// newStr1.localeCompare(newStr3); // -1
 
-console.log(str1.charCodeAt(0)); // 104
-
-console.log(str1.charCodeAt(5)); // 44
-
-
-let str1 = "H";
-let str2 = "ello";
-let strValue = str1 + str2;
-
-let starValue = str1.concat(str2);
-console.log(starValue);
-
-let mystr = "www.ijse.lk";
-mystr.startsWith("www"); // true
-mystr.startsWith("https"); // false
-
-mystr.endsWith("com"); // false
-mystr.endsWith("lk"); // true
-
-let newStr1 = Hello;
-let newStr2 = Hello;
-let newStr3 = Hii;
-
-newStr1.localeCompare(newStr2); // 0
-newStr1.localeCompare(newStr3); // -1
+let mystr = "A,B,C,D,E,F";
+mystr = mystr.replace(",","/");
+console.log(mystr); // A/B,C,D,E,F
+mystr = mystr.replaceAll(",","/");
+console.log(mystr); // A/B/C/D/E/F
 
 
